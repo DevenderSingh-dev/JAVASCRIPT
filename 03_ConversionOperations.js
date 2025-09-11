986c7bef4d9a9cb1242becfd43ca2fa9ed0e9afc
DevenderSingh-dev <devendersingh2139@gmail.com>
2025-09-11 23:22:42 +0530

    CHANGED

--- 03_ConversionOperations.js
+++ 03_ConversionOperations.js
@@ -1,4 +1,4 @@
-// let score = 33;
+let score = 33;
 // let score = "33";
 // let score = "33abc";
 // let score = null;
@@ -10,7 +10,7 @@
 // console.log(typeof score)
 // console.log(typeof(score))
 
-// let valueInNumber = Number(score)
+let valueInNumber = Number(score)
 // console.log(typeof valueInNumber)
 
 // console.log(valueInNumber)
@@ -21,19 +21,19 @@
 // let isLoggedIn = "Rohit" => trur
 
 // using below method we can change/conversion of decalre variable by number to string to boolean and after that we can see to print them in console
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
 
-let someNumber = 33
+// let someNumber = 33
 // console.log(typeof someNumber)
-let stringNumber = String(someNumber)
+// let stringNumber = String(someNumber)
 // console.log(stringNumber)
 // console.log(typeof stringNumber)
 
 // Operations
 
-let value = 3
-let negValue = -value
+// let value = 3
+// let negValue = -value
 // console.log(negValue);
 
 // console.log(2+2);
@@ -43,9 +43,9 @@ let negValue = -value
 // console.log(2/3);
 // console.log(2%3);
 
-let str1 = "Hello";
-let str2 = " Everyone";
-let str3 = str1 + str2;
+// let str1 = "Hello";
+// let str2 = " Everyone";
+// let str3 = str1 + str2;
 // console.log(str3);
 
 // console.log("1" + 2);
@@ -54,6 +54,6 @@ let str3 = str1 + str2;
 // console.log(1 + 2 + "2");
 // console.log((3+4) * 5 % 3);
 
-let gameCounter = 100
-++gameCounter;
-console.log(gameCounter);
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
