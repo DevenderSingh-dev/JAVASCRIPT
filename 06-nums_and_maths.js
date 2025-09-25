e9459805235e281004e492ccdf1b1fcda8ccd3c4
DevenderSingh-dev <devendersingh2139@gmail.com>
2025-09-25 20:19:46 +0530

    change

---
+++ 06-nums_and_maths.js
@@ -0,0 +1,14 @@
+const number = 400
+
+//  console.log(typeof number);
+//  console.log(number);
+
+const balance = new Number (100)
+
+// console.log(typeof balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+  const otherNumber = 123.32342
+  console.log(otherNumber.toPrecision(3));
+
